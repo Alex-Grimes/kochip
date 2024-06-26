36cdacbf47f052e5e0868bc4b4ad347abc9cb387
alex <alexgrimes.se@gmail.com>
2024-06-25 20:35:05 -0400

    display driver implemented using Swing

--- src/main/kotlin/org/kochip/Main.kt
+++ src/main/kotlin/org/kochip/Main.kt
@@ -1,5 +1,26 @@
 package org.kochip
 
+import org.kochip.drivers.CHIP8_HEIGHT
+import org.kochip.drivers.CHIP8_WIDTH
+import org.kochip.drivers.DisplayDriver
+import javax.swing.JFrame
+import javax.swing.WindowConstants
+
 fun main() {
-    println("Hello World!")
+
+    val display = DisplayDriver()
+    display.pixels = Array(CHIP8_HEIGHT) { IntArray(CHIP8_WIDTH) }
+
+    val frame = JFrame("CHIP-8")
+    frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+    frame.contentPane.add(display)
+    frame.pack()
+    frame.isVisible = true
+
+    display.repaint()
+
+    while (true) {
+        display.repaint()
+        Thread.sleep(1000/60)
+    }
 }
--- src/main/kotlin/org/kochip/drivers/Display.kt
+++ src/main/kotlin/org/kochip/drivers/Display.kt
@@ -1,4 +1,35 @@
 package org.kochip.drivers
 
-class Display {
+import javax.swing.JFrame
+import javax.swing.JPanel
+import java.awt.*
+import javax.swing.WindowConstants
+
+const val SCALE = 20
+const val CHIP8_WIDTH = 64
+const val CHIP8_HEIGHT = 32
+
+
+class DisplayDriver : JPanel() {
+    init {
+        preferredSize = Dimension(CHIP8_WIDTH * SCALE, CHIP8_HEIGHT * SCALE)
+    }
+
+    var pixels: Array<IntArray>? = null
+
+    override fun paintComponent(g: Graphics?) {
+        super.paintComponent(g)
+        val g2d = g as Graphics2D
+
+        pixels?.let {
+            for (y in it.indices) {
+                for (x in it[y].indices){
+                    val color = if (it[y][x] == 0) Color.BLACK else Color.WHITE
+                    g2d.color = color
+                    g2d.fillRect(x* SCALE, y* SCALE, SCALE, SCALE)
+                }
+            }
+        }
+    }
 }
+
